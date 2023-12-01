c6a9a8db5a81aafca949d779e8d8c97c3661a2f9
DivinPrince <dpcode36@gmail.com>
2023-12-01 23:09:10 +0200

    init

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -3,25 +3,25 @@ import prismadb from "@/lib/prismadb";
 import { ProductForm } from "./components/product-form";
 
 const ProductPage = async ({
-  params
+  params,
 }: {
-  params: { productId: string, storeId: string }
+  params: { productId: string; storeId: string };
 }) => {
-  const product = await prismadb.product.findUnique({
-    where: {
-      id: params.productId,
-    },
-  });
-
-  return ( 
+  let product = null;
+  if (params.productId != "new") {
+    product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+    });
+  }
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm 
-          initialData={product}
-        />
+        <ProductForm initialData={product} />
       </div>
     </div>
   );
-}
+};
 
 export default ProductPage;
