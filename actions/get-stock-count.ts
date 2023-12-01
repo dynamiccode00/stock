80f11dd6909e44941d030849b6dd2a9577abb872
DivinPrince <dpcode36@gmail.com>
2023-12-01 20:40:10 +0200

    ff

--- actions/get-stock-count.ts
+++ actions/get-stock-count.ts
@@ -1,10 +1,9 @@
 import prismadb from "@/lib/prismadb";
 
 export const getStockCount = async (storeId: string) => {
-  const stockCount = await prismadb.product.count({
+  const stockCount = await prismadb.products.count({
     where: {
       storeId,
-      isArchived: false,
     }
   });
 
