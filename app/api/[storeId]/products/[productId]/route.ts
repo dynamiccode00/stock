bb19345787da854882bd5b6826bf880f8cd71859
DivinPrince <dpcode36@gmail.com>
2023-12-01 21:18:49 +0200

    codify

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -16,12 +16,6 @@ export async function GET(
       where: {
         id: params.productId
       },
-      include: {
-        images: true,
-        category: true,
-        size: true,
-        color: true,
-      }
     });
   
     return NextResponse.json(product);
@@ -80,38 +74,21 @@ export async function PATCH(
 
     const body = await req.json();
 
-    const { name, price, categoryId, images, colorId, sizeId, isFeatured, isArchived } = body;
+    const { name, price, stockCount } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 403 });
     }
 
-    if (!params.productId) {
-      return new NextResponse("Product id is required", { status: 400 });
-    }
-
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
     }
-
-    if (!images || !images.length) {
-      return new NextResponse("Images are required", { status: 400 });
+    if (!stockCount) {
+      return new NextResponse("stockCount id is required", { status: 400 });
     }
 
-    if (!price) {
-      return new NextResponse("Price is required", { status: 400 });
-    }
-
-    if (!categoryId) {
-      return new NextResponse("Category id is required", { status: 400 });
-    }
-
-    if (!colorId) {
-      return new NextResponse("Color id is required", { status: 400 });
-    }
-
-    if (!sizeId) {
-      return new NextResponse("Size id is required", { status: 400 });
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
     }
 
     const storeByUserId = await prismadb.store.findFirst({
@@ -132,29 +109,13 @@ export async function PATCH(
       data: {
         name,
         price,
-        categoryId,
-        colorId,
-        sizeId,
-        images: {
-          deleteMany: {},
-        },
-        isFeatured,
-        isArchived,
+        stockCount
       },
     });
 
     const product = await prismadb.product.update({
       where: {
         id: params.productId
-      },
-      data: {
-        images: {
-          createMany: {
-            data: [
-              ...images.map((image: { url: string }) => image),
-            ],
-          },
-        },
       },
     })
   
