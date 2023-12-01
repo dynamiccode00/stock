8d53002fdec597fbe63a57d1124c1c5739c57070
DivinPrince <dpcode36@gmail.com>
2023-12-01 20:25:17 +0200

    init

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+
+export type ProductColumn = {
+  id: string
+  name: string;
+  price: string;
+  stockQuantity: number;
+  createdAt: string;
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "stockQuantity",
+    header: "StockQuantity",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+];
