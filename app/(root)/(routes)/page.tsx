f439ad1d8d1ee187806da6a6680c75568a38a58c
DivinPrince <dpcode36@gmail.com>
2023-12-07 13:16:33 +0200

    updat: homepage

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 export const dynamic = 'force-dynamic'
 
 const SetupPage = () => {
+  const [isMouted, setIsMouted] = useState(false)
+  useEffect(() => {
+    setIsMouted(true)
+  }, [])
+  
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
-  useEffect(() => {
-    if (!isOpen) {
-      onOpen();
-    }
-  }, [isOpen, onOpen]);
+
+  if (!isOpen) {
+    onOpen();
+  }
 
   return null;
 };
- 
+
 export default SetupPage;
