d3376e8760e4a3ed31c946ef9b2479e851dc287c
DivinPrince <dpcode36@gmail.com>
2023-12-08 11:00:23 +0200

    in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,31 +1,27 @@
 import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
-import { auth } from '@clerk/nextjs';
 
 export default async function SetupLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { userId,user } = auth();
-  let id = 'user_2YxJdWWmZfzFMbi192obx0KMBbY'
+  const { userId } = auth();
 
   if (!userId) {
     redirect('/sign-in');
   }
 
-  const store = await prismadb.store.findFirst({
-    where: {
-      id,
-    }
-  });
+  const store = await prismadb.store.findFirst();
 
-  if (userId == 'user_2ZFkhqgvmyN8kO9H7HyhS8FRYIN' && store) {
-    redirect(`/${store?.id}/seller`);
-  };
-  if (userId == 'user_2YxJdWWmZfzFMbi192obx0KMBbY' && store) {
-    redirect(`/${store?.id}`);
+  if (store) {
+    if (userId == 'user_2ZFkhqgvmyN8kO9H7HyhS8FRYIN') {
+      redirect(`/${store.id}/seller`);
+    }else{
+      redirect(`/${store.id}`);
+    }
   };
 
   return (
